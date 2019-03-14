3c7c708edf19c53866ab5313f0d58fbbf53c3cbb
merry-lab-3791 <emilyroulund@gmail.com>
2019-03-14 06:14:23 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,34 @@
+//for ([initialization]; [condition]; [iteration]) {[loopBody];}
+// initialition = expression or variable declaration; used to initialize a counter variable; may optionally declare new variables with the var keyword 
+//condition: expression evaluate before each loop iteration; if expression evaluates to true, statement is executed
+//iteration: statement exectured at the end of each iteration; typically involves incrementing or decrementing a counter, bringing loop closer to end 
+//loopBody: code that runs on every iteration as long as condition evalurates to true 
+//USE THIS LOOP WHEN YOU KNOW HOW MANY YIMES YOU WANT THE LOOP TO RUN- FOR EX, WHEN YOU HAVE AN ARRAY OF KNOWN SIZE 
+
+
+function forLoop (array){
+  for (let i = 0; i<25; i++ )
+  return ("I am" + i + "strange loop.")
+   //  if (i = 1) 
+   //  console.log ("I am" + ${i} + "strange loop.")
+    //if (i>1) 
+      //console.log ("I am" + ${i} + "strange loops")
+}
+
+function forLoop (array) {
+  if (i===1){
+    return ("I am" + ${i} + "strange loop.")
+      const [array, t] = makeArray()
+      const strangeArray = forLoop(array)}
+
+
+function WhileLoop (n){ 
+  while (n > 0) {
+  console.log(--countdown);
+}
+}
+  
+//take integer as argument; 
+//use incrementVariable() function (copy from README) in the condition and console log "I run once regardless." while incrementCVariable() returns a number less than the parameter received. 
+//(Your condition might look somethings like incrementVariable() < num.) 
+//should console log when receiving 0 as a paramenter because the do-while runs before the condition is checked 
